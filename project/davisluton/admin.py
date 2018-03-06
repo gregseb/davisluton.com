6480845c6913eb30c37a5ed4e255355793399ec5
Greg Sebastian <greg+github@sebastiantech.ca>
2018-03-05 20:44:28 -0800

    Added Portfolio and Portfolio image models to backend

--- project/davisluton/admin.py
+++ project/davisluton/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Portfolio, PortfolioImage
 
-# Register your models here.
+
+class PortfolioImageInline(admin.TabularInline):
+    model = PortfolioImage
+    exclude = ['status_changed', 'created', 'modified', 'is_removed']
+    extra = 3
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    exclude = ['status_changed', 'created', 'modified', 'is_removed']
+    inlines = [PortfolioImageInline]
--- project/davisluton/models.py
+++ project/davisluton/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from model_utils.models import TimeStampedModel, StatusModel, SoftDeletableModel
+from model_utils.managers import SoftDeletableManager
+from model_utils import Choices
 
-# Create your models here.
+
+class BaseModel(TimeStampedModel, StatusModel, SoftDeletableModel):
+    STATUS = Choices('draft', 'published')
+
+    objects = SoftDeletableManager()
+
+    class Meta:
+        abstract = True
+
+
+class Portfolio(BaseModel):
+    image = models.ImageField()
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class PortfolioImage(BaseModel):
+    image = models.ImageField()
+    description = models.TextField(blank=True, null=True)
+
+    portfolio = models.ForeignKey('Portfolio', on_delete=models.CASCADE)
