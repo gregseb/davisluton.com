49ea97e1711f3ce26b3987f71a0c6e0517eaf472
Greg Sebastian <greg@sebastiantech.ca>
2018-04-27 19:24:35 -0700

    Edited exmaple.local.py so that it will work with docker out of the box

--- config/settings/example.local.py
+++ config/settings/example.local.py
@@ -4,4 +4,19 @@ from .base import *
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='*1hvq1ebcfy2w(bj@efvv-gu@*i^%k-=73@1mrqxys1mru&gyt')
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+# Edit this to contain only the hostnames you want to be able to access this site.
+ALLOWED_HOSTS = ['127.0.0.1', 'db', 'web', 'nginx']
+
+# An unsafe middleware for if you have cors issues in development.
+# MIDDLEWARE.append('project.davisluton.middleware.dev_cors_middleware')
+
+# Database settings for connecting to the docker postgres image.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
